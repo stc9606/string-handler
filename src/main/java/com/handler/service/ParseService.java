3c3d4679ffe7719e66fea818aa3c639a40fbe5b1
s_chuli <stc9606@naver.com>
2022-03-17 00:09:32 +0900

    settings : 컨트롤러, 서비스, DAO 세팅

---
+++ src/main/java/com/handler/service/ParseService.java
@@ -0,0 +1,13 @@
+package com.handler.service;
+
+import com.handler.dao.ParseRequest;
+import com.handler.dao.ParseResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParseService {
+    public ParseResponse parse(ParseRequest request) {
+
+        return null;
+    }
+}
--- src/test/java/com/handler/StringHandlerApplicationTests.java
+++ src/test/java/com/handler/StringHandlerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.handler.demo;
+package com.handler;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
