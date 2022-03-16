661275ef876d92c88d723a541d6d1590002c65f5
s_chuli <stc9606@naver.com>
2022-03-16 23:49:23 +0900

    Project Setting

---
+++ src/main/java/com/handler/demo/StringHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.handler.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringHandlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringHandlerApplication.class, args);
+    }
+
+}
