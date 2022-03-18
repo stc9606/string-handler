78ffee3937d58bf21bb38e73231bdbfc8167c588
s_chuli <stc9606@naver.com>
2022-03-18 22:20:15 +0900

    feat: UrlConnect Add

--- src/main/java/com/handler/parse/util/UrlConnector.java
+++ src/main/java/com/handler/parse/util/UrlConnector.java
@@ -1,5 +1,21 @@
 package com.handler.parse.util;
 
 // static method vs @Component
+
+import org.jsoup.Jsoup;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
 public class UrlConnector {
+
+    public String getHtml(final String url) {
+        //jsoup -> 장/단점 조사
+        try {
+            return Jsoup.connect(url).get().html();
+        } catch (IOException e) {
+            throw new IllegalArgumentException("접근할 수 없는 Url입니다.");
+        }
+    }
 }
