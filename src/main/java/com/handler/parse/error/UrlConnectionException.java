cb109f9de93b1bcf4f0e51feb7be45ccd6ff50d7
s_chuli <stc9606@naver.com>
2022-03-22 01:07:39 +0900

    feat: GlobalExceptionHandler 추가

---
+++ src/main/java/com/handler/parse/error/UrlConnectionException.java
@@ -0,0 +1,8 @@
+package com.handler.parse.error;
+
+public class UrlConnectionException extends IllegalArgumentException {
+
+    public UrlConnectionException(String s) {
+        super(s);
+    }
+}
