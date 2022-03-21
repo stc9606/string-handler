f79b62b08d9d1d2bd6f5d03c74e52f7fcc16fc27
s_chuli <stc9606@naver.com>
2022-03-22 00:38:35 +0900

    setting: swagger 셋팅

--- src/main/java/com/handler/parse/dao/ParseRequest.java
+++ src/main/java/com/handler/parse/dao/ParseRequest.java
@@ -1,5 +1,6 @@
 package com.handler.parse.dao;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +8,13 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public class ParseRequest {
+
+    @Schema(description = "url", defaultValue = "https://www.naver.com")
     private final String url;
+
+    @Schema(description = "노출유형", defaultValue = "REMOVE_HTML")
     private final ExposureType exposureType;
+
+    @Schema(description = "출력 묶음 단위", defaultValue = "10")
     private final Integer unitCount;
 }
